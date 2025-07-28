892ee66a3b73cbc4c1be84cb342d2f9e6855c063
halilayan <halilayan06@gmail.com>
2025-07-29 01:01:28 +0300

    edit_debounce

--- VHDL ile Basys3 Programlama Serisi - #8 (butonlar ile LED kontrol)/sources_1/new/btn_led.vhd
+++ VHDL ile Basys3 Programlama Serisi - #8 (butonlar ile LED kontrol)/sources_1/new/btn_led.vhd
@@ -1,4 +1,5 @@
 
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -20,40 +21,36 @@ architecture Behavioral of btn_led is
 
     constant max_counter    : natural := 1_000_000-1;
     signal counter          : unsigned (19 downto 0) := (others => '0');
-    signal debounce         : std_logic := '0';
+    signal debounce_btnR    : std_logic := '0';
     
-
+    
+    signal debounce_btnL    : std_logic := '0';
     signal prev_btnL        : std_logic := '0';
     signal led_state        : std_logic := '0';
 
 
 begin
 
---zamanlama y�ntemi
+--zamanlama yöntemi
     process(clk)
         begin
             if rising_edge(clk) then
                 if counter = max_counter then
                     counter <= (others => '0');
-                    debounce <= btnR;
+                    debounce_btnR <= btnR;
+                    debounce_btnL <= btnL;
                 else 
                     counter <= counter + 1;    
                 end if;
-            end if;
-    end process;
-    
---edge detection
-    process(clk)
-        begin
-            if rising_edge(clk) then
-                if btnL = '1' and prev_btnL = '0' then
+                if debounce_btnL = '1' and prev_btnL = '0' then
                     led_state <= not led_state;
                 end if;
-              prev_btnL <= btnL;  
+                prev_btnL <= debounce_btnL;
             end if;
-    end process;    
+    end process;
+   
     
-    led0    <= '1' when debounce    = '1' else '0';
+    led0    <= debounce_btnR;
     led15   <=  led_state;
 
 end Behavioral;
